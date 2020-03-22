38862d54c0420ef03f74517b250fb96a6a13d1ca
Mrmi <56785744+Mrmi310@users.noreply.github.com>
2020-03-22 21:47:15 +0100

    Update LeetCode Reverse Linked List.cpp

--- LinkedList/LeetCode Reverse Linked List.cpp
+++ LinkedList/LeetCode Reverse Linked List.cpp
@@ -1,4 +1,14 @@
 /*
+C++ iterative and recursive solution for https://leetcode.com/problems/reverse-linked-list/submissions/
+Possibly not quite the best solutions.
+Iteratively: 
+Runtime: 8 ms, faster than 94.31% of C++ online submissions for Reverse Linked List.
+Memory Usage: 7.7 MB, less than 100.00% of C++ online submissions for Reverse Linked List.
+
+Recursively: 
+Runtime: 12 ms, faster than 40.51% of C++ online submissions for Reverse Linked List.
+Memory Usage: 7.6 MB, less than 100.00% of C++ online submissions for Reverse Linked List.
+
 206. Reverse Linked List
 Difficulty: Easy
 
